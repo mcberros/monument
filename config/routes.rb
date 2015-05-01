9f770b64b161119741e42fb01c277b137a840648
mcberros <mcberros@gmail.com>
2015-05-01 13:13:57 +0200

    Created sign up out login system for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  root :to => 'users#index'
+
+  resources :user_sessions
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
+
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
