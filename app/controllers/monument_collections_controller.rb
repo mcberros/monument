bccafe233811b9067ec8681672a4a4b8e90a144e
mcberros <mcberros@gmail.com>
2015-05-01 15:12:32 +0200

    Created actions and views for monument collections

--- app/controllers/monument_collections_controller.rb
+++ app/controllers/monument_collections_controller.rb
@@ -5,20 +5,40 @@ class MonumentCollectionsController < ApplicationController
   end
 
   def show
+    @monument_collection = MonumentCollection.where(id: params[:id])
   end
 
   def new
+    @monument_collection = MonumentCollection.new
   end
 
   def create
+    @monument_collection = MonumentCollection.new monument_collection_params
+    if @monument_collection.save
+      redirect_to @monument_collection, notice: 'Collection created'
+    else
+      render :new
+    end
   end
 
   def edit
+    @monument_collection = MonumentCollection.where(id: params[:id])
   end
 
   def update
+    @monument_collection = MonumentCollection.update monument_collection_params
+    if @monument_collection.save
+      redirect_to @monument_collection, notice: 'Collection updated'
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+  def monument_collection_params
+    params.require(:monument_collection).permit(:name)
   end
 end
