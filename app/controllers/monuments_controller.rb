2811be0d060bb20c27e89157de42d41df166aff9
mcberros <mcberros@gmail.com>
2015-05-07 20:06:35 +0200

    Added cancel button to the multi step form

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -30,6 +30,12 @@ class MonumentsController < ApplicationController
   end
 
   def create
+    if params[:cancel_button]
+      session[:monument_step] = session[:monument_params] = session["monument_files"] = nil
+      redirect_to monuments_path
+      return
+    end
+
     is_finished = false
 
     images = []
