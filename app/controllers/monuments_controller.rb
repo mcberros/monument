a9b8f3877e3be84c8623b4162bdef127925e4f47
mcberros <mcberros@gmail.com>
2015-05-12 22:29:54 +0200

    Refactor

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -22,8 +22,7 @@ class MonumentsController < ApplicationController
     if MonumentCollection.count == 0
       redirect_to new_monument_collection_path, notice: "Please, create first a monument collection"
     else
-      session[:monument_params] ||= {}
-      session["monument_files"] ||= {}
+      init_session
       @monument = Monument.new(session[:monument_params])
       @monument.current_step = session[:monument_step]
     end
@@ -38,8 +37,7 @@ class MonumentsController < ApplicationController
   def edit
     check_forbidden
 
-    session[:monument_params] ||= {}
-    session["monument_files"] ||= {}
+    init_session
     @monument.attributes = session[:monument_params]
     @monument.current_step = session[:monument_step]
   end
@@ -64,12 +62,10 @@ class MonumentsController < ApplicationController
 
   def multistep_control(form_to_render, notice_msg)
     if params[:cancel_button]
-      session[:monument_step] = session[:monument_params] = session["monument_files"] = nil
+      clear_session
       redirect_to monuments_path
       return
     end
-
-    is_finished = false
 
     images = is_there_picture_params? ? move_images_from_params_to_array : []
 
@@ -80,24 +76,43 @@ class MonumentsController < ApplicationController
     store_in_session_uploaded_images_path(images)
     populate_model_pictures_with_files_in_session
 
+    is_saved = perform_step_action
+
+    unless is_saved
+      render form_to_render
+    else
+      clear_session
+      redirect_to monuments_path, notice: notice_msg
+    end
+  end
+
+  def perform_step_action
+    is_saved = false
+
     @monument.current_step = session[:monument_step]
+
     if @monument.valid?
-      if params[:previous_button]
-        @monument.previous_step
-      elsif @monument.last_step?
-        is_finished = @monument.save
-      else
+      if params[:next_button]
         @monument.next_step
+      elsif params[:previous_button]
+        @monument.previous_step
+      elsif params[:save_button]
+        is_saved = @monument.save
       end
+
       session[:monument_step] = @monument.current_step
     end
 
-    unless is_finished
-      render form_to_render
-    else
-      session[:monument_step] = session[:monument_params] = session["monument_files"] = nil
-      redirect_to monuments_path, notice: notice_msg
-    end
+    is_saved
+  end
+
+  def init_session
+    session[:monument_params] ||= {}
+    session["monument_files"] ||= {}
+  end
+
+  def clear_session
+    session[:monument_step] = session[:monument_params] = session["monument_files"] = nil
   end
 
   def check_forbidden
@@ -112,11 +127,13 @@ class MonumentsController < ApplicationController
 
   def move_images_from_params_to_array
     # TODO: We need to test if the picture has image
+    #TODO: pero no incluir las imagenes de momumentos con params[:monument][:pictures_attributes]._destroy es 1
     params[:monument]["pictures_attributes"]
         .map{|k,picture| picture.delete("image")}
   end
 
   def merge_session_with_monument_params
+    #TODO: borrar de session[:monument_params] y de session[:monument_files] pictures con _destroy igual 1
     session[:monument_params].deep_merge!(monument_params)
   end
 
